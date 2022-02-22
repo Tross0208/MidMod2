29f17ff6e5a5e1bf7e6b206c4d02d4c459853727
Tyler Ross <tross020890@gmail.com>
2022-02-22 10:39:21 -0600

    Feat: studio show links to studio index

---
+++ app/controllers/studios_controller.rb
@@ -0,0 +1,9 @@
+class StudiosController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get '/studios', to: 'studios#show'
+get '/studios/movies', to: 'studios#index'
+
+
+
+
 end
